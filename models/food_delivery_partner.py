87454915de299cf2d58f1d0b910d74e1659545ea
Harsh Modi <hamo@odoo.com>
2023-01-24 18:53:40 +0530

    [IMP] food: improved the structure of delivery_partner
    Removed the delegation inheritance as it was creating multiple records for
    delivery partner.
    
    Added current order assigned to the delivery partner field
    which will be ready only
    
    Added and improved the compute methods in food_order on state and
    delivery_partner_id change
    
    Available delivery partner can be auto-assigned after accepting the order.

--- models/food_delivery_partner.py
+++ models/food_delivery_partner.py
@@ -1,7 +1,9 @@
 from odoo import fields,models
 
 class FoodDeliveryPartner(models.Model):
-    _inherits={"res.users":"user_id"}
     _name="food.delivery.partner"
-    user_id=fields.Many2one("res.users")
+    name=fields.Char(required=True)
+    active=fields.Boolean(default=True)
+    all_order_ids=fields.One2many("food.order","delivery_partner_id")
     pay_type=fields.Selection([('fixed','Fixed'),('per_km','Per Kilometer'),('per_delivery','Per Delivery')])
+    current_order_assigned=fields.Many2one("food.order",readonly=True)
--- models/food_order.py
+++ models/food_order.py
@@ -1,13 +1,13 @@
-from odoo import fields,models,api
-
+from odoo import fields,models,api,_
+from odoo.exceptions import UserError,ValidationError
 class FoodOrder(models.Model):
     _name="food.order"
     _description="Food Description"
     customer_id=fields.Many2one("res.partner")
     resturant_id=fields.Many2one("food.resturant")
     order_line_ids=fields.One2many("food.order.line","order_id")
-    state=fields.Selection([('new','New'),('accepted','Accepted'),('dp_assigned','Delivery Partner Assigned'),('picked','Picked'),('delivered','Delivered'),('canceled','Canceled')],default='new',compute="_onchange_delivery_partner",readonly=False,store=True)
-    delivery_partner_id=fields.Many2one("res.users")
+    state=fields.Selection([('new','New'),('accepted','Accepted'),('dp_assigned','Delivery Partner Assigned'),('picked','Picked'),('delivered','Delivered'),('canceled','Canceled')],default='new',compute="_onchange_delivery_id",readonly=False,store=True)
+    delivery_partner_id=fields.Many2one("food.delivery.partner",compute="_onchange_state",readonly=False,store=True)
     total_price=fields.Float(compute="_compute_total_price",default=0)
     @api.depends("order_line_ids")
     def _compute_total_price(self):
@@ -20,7 +20,27 @@ class FoodOrder(models.Model):
     def action_state_canceled(self):
         for record in self:
             record.state='canceled'
-    @api.depends("delivery_partner_id")
-    def _onchange_delivery_partner(self):
+    @api.depends("state")
+    def _onchange_state(self):
         for record in self:
-            record.state='dp_assigned' if record.delivery_partner_id else record.state
+            if record.state=='accepted':
+                dp=self.env['food.delivery.partner'].search([('current_order_assigned','=',False)])
+                if not dp:
+                    raise UserError(_("All delivery partner are busy please try after sometime!"))
+                record.delivery_partner_id=dp[0].id
+                dp[0].current_order_assigned=record.id
+                record.state='dp_assigned'
+            if record.state=='delivered':
+                record.delivery_partner_id.current_order_assigned=False
+    @api.depends("delivery_partner_id")
+    def _onchange_delivery_id(self):
+        for record in self:
+            dp=record.delivery_partner_id
+            dp.current_order_assigned=record.id
+            record.state='dp_assigned'
+    @api.constrains("delivery_partner_id")
+    def _check_delivery_partner(self):
+        for record in self:
+            cao=record.delivery_partner_id.current_order_assigned
+            if cao and cao!=record:
+                raise ValidationError(_("Delivery partner cannot be assigned"))
