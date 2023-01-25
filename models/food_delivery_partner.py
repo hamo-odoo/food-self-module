150c4863cd479395d1e11ffc6ef16b97ad6f7dee
Harsh Modi <hamo@odoo.com>
2023-01-25 18:55:04 +0530

    [IMP] food: improved UI and functional flow of food_rder
    Added action button for order picked and delivered in food_delivery_partner
    
    Reduced also and removed unwanted code which is not now required in the
    functional of the module in food_order
    
    Once the ordered is delivered the current_order_assigned is set to False

--- models/food_delivery_partner.py
+++ models/food_delivery_partner.py
@@ -7,3 +7,12 @@ class FoodDeliveryPartner(models.Model):
     all_order_ids=fields.One2many("food.order","delivery_partner_id")
     pay_type=fields.Selection([('fixed','Fixed'),('per_km','Per Kilometer'),('per_delivery','Per Delivery')])
     current_order_assigned=fields.Many2one("food.order",readonly=True)
+    current_order_state=fields.Selection(related="current_order_assigned.state")
+    def action_order_state_picked(self):
+        for record in self:
+            record.current_order_assigned.state='picked'
+    def action_order_state_delivered(self):
+        for record in self:
+            record.current_order_assigned.state='delivered'
+            record.current_order_assigned=False
+            
--- models/food_order.py
+++ models/food_order.py
@@ -7,8 +7,13 @@ class FoodOrder(models.Model):
     resturant_id=fields.Many2one("food.resturant")
     order_line_ids=fields.One2many("food.order.line","order_id")
     state=fields.Selection([('new','New'),('accepted','Accepted'),('dp_assigned','Delivery Partner Assigned'),('picked','Picked'),('delivered','Delivered'),('canceled','Canceled')],default='new',compute="_onchange_delivery_id",readonly=False,store=True)
-    delivery_partner_id=fields.Many2one("food.delivery.partner",compute="_onchange_state",readonly=False,store=True)
+    delivery_partner_id=fields.Many2one("food.delivery.partner",)
     total_price=fields.Float(compute="_compute_total_price",default=0)
+    def name_get(self):
+        res=[]
+        for record in self:
+            res.append((record.id,'Food order id:%s'%record.id))
+        return res
     @api.depends("order_line_ids")
     def _compute_total_price(self):
         for record in self:
@@ -17,21 +22,10 @@ class FoodOrder(models.Model):
     def action_state_accepted(self):
         for record in self:
             record.state='accepted'
+            self.auto_search_partner()
     def action_state_canceled(self):
         for record in self:
             record.state='canceled'
-    @api.depends("state")
-    def _onchange_state(self):
-        for record in self:
-            if record.state=='accepted':
-                dp=self.env['food.delivery.partner'].search([('current_order_assigned','=',False)])
-                if not dp:
-                    raise UserError(_("All delivery partner are busy please try after sometime!"))
-                record.delivery_partner_id=dp[0].id
-                dp[0].current_order_assigned=record.id
-                record.state='dp_assigned'
-            if record.state=='delivered':
-                record.delivery_partner_id.current_order_assigned=False
     @api.depends("delivery_partner_id")
     def _onchange_delivery_id(self):
         for record in self:
@@ -44,3 +38,11 @@ class FoodOrder(models.Model):
             cao=record.delivery_partner_id.current_order_assigned
             if cao and cao!=record:
                 raise ValidationError(_("Delivery partner cannot be assigned"))
+    def auto_search_partner(self):
+        for record in self:
+            dp=self.env['food.delivery.partner'].search([('current_order_assigned','=',False)])
+            if not dp:
+                raise UserError(_("All delivery partner are busy please try after sometime!"))
+            record.delivery_partner_id=dp[0].id
+            dp[0].current_order_assigned=record.id
+            record.state='dp_assigned'
